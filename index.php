a631dbbfb1a3b155cca4326110585506229830d9
Cesar Dutten <cesar.dutten@extendeal.com>
2018-06-13 14:34:34 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: extendeal
+ * Date: 13/06/18
+ * Time: 14:30
+ */
---
+++ src/cdutten/invoices-generator/Invoice.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: extendeal
+ * Date: 13/06/18
+ * Time: 14:37
+ */
+
+namespace cdutten\generator;
+
+
+class Invoice
+{
+
+}
